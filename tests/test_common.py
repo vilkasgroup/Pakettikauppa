28f56ec95a342b5019d763b08999a188a0b3bf39
Tipi <tipi@vilkas.fi>
2018-01-25 16:29:15 +0200

    added test case for getting api config

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,5 +1,5 @@
 import unittest
-from pakettikauppa.merchant import decode_pdf_content
+from pakettikauppa.merchant import decode_pdf_content, PkMerchant
 
 
 class TestGeneral(unittest.TestCase):
@@ -15,7 +15,12 @@ class TestGeneral(unittest.TestCase):
 
     def test_empty_pdf_content(self):
         with self.assertRaises(ValueError):
-            decode_pdf_content('')
+            decoded_pdf_content_string = decode_pdf_content('')
+
+    def test_get_api_conf(self):
+        merchant = PkMerchant(1)
+        h_config = merchant.get_api_config('list_customer')
+        self.assertIsNotNone(h_config)
 
 
 if __name__ == '__main__':
